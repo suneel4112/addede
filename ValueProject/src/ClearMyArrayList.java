3226c20366a4535d0da0b8aba165a681f3b04697
sknunna <sknunna@A4112E1D.corp.ads.valuelabs.net>
2014-11-11 16:13:02 +0530

    added new project.
    Change-Id: Ide0e7d43428db1270e06b88774cfae064c0bf888

---
+++ ValueProject/src/ClearMyArrayList.java
@@ -0,0 +1,28 @@
+ 
+import java.util.ArrayList;
+ 
+public class ClearMyArrayList {
+ 
+    public static void main(String a[]){
+        ArrayList<String> arrl = new ArrayList<String>();
+        //adding elements to the end
+        arrl.add("test");
+        arrl.add("revanth");
+        arrl.add("changed");
+        arrl.add("Random");
+        System.out.println("Actual ArrayList:"+arrl);
+        arrl.clear();
+        // System.out.println("After clear ArrayList:"+arrl);
+        System.out.println("last line fo method");
+        System.out.println("Last but one line fo method");
+        //System.out.println("Last but one line fo method");
+        //System.out.println("Last but one line fo method");
+        //System.out.println("Last but one line fo method");
+        
+        // suneel added new comment
+         
+
+        // System.out.println("Last but one line fo method");
+    
+    }
+}
---
+++ ValueProject/src/FindDups2.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+public class FindDups2 {
+    public static void main(String[] args) {
+        Set<String> uniques = new HashSet<String>();
+        Set<String> dups    = new HashSet<String>();
+
+        for (String a : args)
+            if (!uniques.add(a))
+                dups.add(a);
+
+        // Destructive set-difference usage
+        uniques.removeAll(dups);
+
+        System.out.println("Unique words:    " + uniques);
+        System.out.println("Duplicate words: " + dups);
+        System.out.println("This is Sunil GIT");
+    }
+}
